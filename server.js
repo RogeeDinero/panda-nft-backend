7aaf8b79e828a260a57b3a5853d8ad1089d97415
RogeeDinero <90359409+RogeeDinero@users.noreply.github.com>
2025-12-19 03:16:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,16 +11,4 @@ app.use(cors({
 
 app.use(express.json());
 
-function deserializeAtomicData(serializedData) {
-  // Simple AtomicAssets deserializer for immutable_data
-  let data = {};
-  for (let i = 0; i < serializedData.length; i += 2) {
-    const key = String.fromCharCode(serializedData[i]);
-    const valueLength = serializedData[i + 1];
-    const value = serializedData.slice(i + 2, i + 2 + valueLength).toString();
-    data[key] = value;
-  }
-  return data;
-}
-
-app.get('/api/nfts', async (req, res) =>
+app.get('/api/nfts
